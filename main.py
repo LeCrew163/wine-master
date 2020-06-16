21d6f1f1e4704ddccf248299de0ea1423ad3aad1
Nikolay <nikolay@MBP-Nikolaj.fritz.box>
2020-06-16 20:04:51 +0200

    v.2

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import datetime
 import pandas
+import collections
 from pprint import pprint
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 def convert_wine_assortment_excel_to_dict():
-    excel_wine_assortment = pandas.read_excel('wine.xlsx', sheet_name='Лист1')
+    excel_wine_assortment = pandas.read_excel('wine2.xlsx', sheet_name='Лист1')
     wine_dict = excel_wine_assortment.to_dict(orient='record')
     return wine_dict
+
+def sort_wine_by_category(wine_dict):
+    sorted_wine_by_category = collections.defaultdict(list)
+    for number, __ in enumerate(wine_dict):
+        category = wine_dict[number]['Категория']
+        sorted_wine_by_category[category]
+        sorted_wine_by_category[category].append(wine_dict[number])
+    return sorted_wine_by_category
 
 def count_company_age():
     now = datetime.datetime.now()
@@ -19,7 +28,8 @@ def count_company_age():
 
 def main():
     actual_company_age = count_company_age()
-    wines = convert_wine_assortment_excel_to_dict()
+    wine_dict = convert_wine_assortment_excel_to_dict()
+    sorted_wine_dict = sort_wine_by_category(wine_dict)
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -30,7 +40,7 @@ def main():
 
     rendered_page = template.render(
         years_amount=actual_company_age,
-        wines=wines
+        sorted_wine_dict=sorted_wine_dict,
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
--- test.py
+++ test.py
@@ -2,19 +2,34 @@ import collections
 import pandas
 from pprint import pprint
 
-wine = pandas.read_excel('wine2.xlsx', sheet_name='Лист1')
-wine_dict = wine.to_dict(orient='record')
+def sort_wine_by_category(wine_dict):
+    sorted_wine_by_category = collections.defaultdict(list)
+    for number, __ in enumerate(wine_dict):
+        category = wine_dict[number]['Категория']
+        sorted_wine_by_category[category]
+        sorted_wine_by_category[category].append(wine_dict[number])
+    return sorted_wine_by_category
+
+excel_wine_assortment = pandas.read_excel('wine2.xlsx', sheet_name='Лист1')
+wine_dict = excel_wine_assortment.to_dict(orient='record')
+
+pprint(sort_wine_by_category(wine_dict))
 
 
-sorted_wine_dict = collections.defaultdict(list)
-sorted_wine_dict['Белые вина']
-sorted_wine_dict['Красные вина']
-sorted_wine_dict['Напитки']
 
-for num, wine_infos in enumerate(wine_dict):
-    sorted_wine_dict['Белые вина'].append(wine_infos)
 
-pprint(sorted_wine_dict)
+
+#wines_by_category = {}
+#for num, __ in enumerate(wine_dict):
+#   category = wine_dict[num]['Категория']
+#   if category not in wines_by_category.keys():
+#        wines_by_category[category] = []
+#        wines_by_category[category].append(wine_dict[num])
+#
+#print(wines_by_category)
+
+
+
 
 
 #white_wines = []
@@ -23,18 +38,17 @@ pprint(sorted_wine_dict)
 #
 #sorted_wine_dict = {
 #    'Белые вина': white_wines,
-#    'Красные вина': red_wines ,
+#    'Красные вина': red_wines,
 #    'Напитки': other_drinks
 #}
 #
 #for num, __ in enumerate(wine_dict):
-#    if wine_dict[num]['Категория'] == 'Белые вина':
+#    category = wine_dict[num]['Категория']
+#    if category == 'Белые вина':
 #        white_wines.append(wine_dict[num])
-#    elif wine_dict[num]['Категория'] == 'Красные вина':
+#    elif category == 'Красные вина':
 #        red_wines.append(wine_dict[num])
 #    else:
 #        other_drinks.append(wine_dict[num])
 #
 #pprint(sorted_wine_dict)
-
-
